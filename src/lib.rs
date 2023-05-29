3b9c54f9ab86a40c9e853fcd91857ab0687f06e2
DoubleDebug <duledodicc@gmail.com>
2023-05-29 14:28:07 +0200

    fix - crop image endpoint error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,97 +1,123 @@
 use image::imageops;
 use std::path::PathBuf;
 use std::process::Command;
-use serde::{Serialize, Deserialize};
+use serde::{ Serialize, Deserialize };
 
 pub mod utils {
-    pub mod file;
-    pub mod validate;
+  pub mod file;
+  pub mod validate;
 }
 use utils::file::file::get_output_path;
 use utils::validate::validate::validate_options;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Point {
-    pub x: u32,
-    pub y: u32,
+  pub x: u32,
+  pub y: u32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ImageSize {
-    pub width: u32,
-    pub height: u32,
+  pub width: u32,
+  pub height: u32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct CropOptions {
-    pub input_file_path: String,
-    pub output_file_path: Option<String>,
-    pub top_left_point: Point,
-    pub size: ImageSize,
+  pub input_file_path: String,
+  pub output_file_path: Option<String>,
+  pub top_left_point: Point,
+  pub size: ImageSize,
+}
+
+#[derive(Serialize)]
+pub struct ApiResponse<T> {
+  pub success: bool,
+  pub message: Option<String>,
+  pub data: Option<T>,
 }
 
 /**
  * Crops image with the given options and returns the file path of the newly created cropped image.
  */
 pub fn crop_image(options: &CropOptions) -> Result<String, String> {
-    validate_options(&options)?;
+  validate_options(&options)?;
 
-    let mut img = image::open(&options.input_file_path).unwrap();
-    let cropped_img = imageops::crop(
-        &mut img,
-        options.top_left_point.x,
-        options.top_left_point.y,
-        options.size.width,
-        options.size.height,
-    );
-
-    let output_file_path;
-    if let Some(output_path) = &options.output_file_path {
-        output_file_path = PathBuf::from(&output_path);
-    } else {
-        output_file_path = get_output_path(&options.input_file_path);
+  let mut img = match image::open(&options.input_file_path) {
+    Ok(img) => img,
+    Err(error) => {
+      return Err(format!("There was an error while opening the image file: {:?}", error));
     }
-    cropped_img.to_image().save(&output_file_path).unwrap();
+  };
+  let cropped_img = imageops::crop(
+    &mut img,
+    options.top_left_point.x,
+    options.top_left_point.y,
+    options.size.width,
+    options.size.height
+  );
 
-    Ok(output_file_path.to_str().unwrap().to_string())
+  let output_file_path;
+  if let Some(output_path) = &options.output_file_path {
+    output_file_path = PathBuf::from(&output_path);
+  } else {
+    output_file_path = get_output_path(&options.input_file_path);
+  }
+  match cropped_img.to_image().save(&output_file_path) {
+    Ok(_) => (),
+    Err(error) => {
+      return Err(format!("There was an error while saving the cropped image: {:?}", error));
+    }
+  }
+
+  Ok(output_file_path.to_str().unwrap().to_string())
 }
 
 /**
  * Crops video with the given options and returns the file path of the newly created cropped video file.
  */
 pub fn crop_video(options: &CropOptions) -> Result<String, String> {
-    // validate_options(&options)?;
+  // validate_options(&options)?;
 
-    let crop_dimensions = format!("crop={}:{}:{}:{}",
-    &options.size.width, &options.size.height, &options.top_left_point.x, &options.top_left_point.y); // width:height:x:y
-    let output_file_path;
-    if let Some(output_path) = &options.output_file_path {
-        output_file_path = PathBuf::from(&output_path);
-    } else {
-        output_file_path = get_output_path(&options.input_file_path);
+  let crop_dimensions = format!(
+    "crop={}:{}:{}:{}",
+    &options.size.width,
+    &options.size.height,
+    &options.top_left_point.x,
+    &options.top_left_point.y
+  ); // width:height:x:y
+  let output_file_path;
+  if let Some(output_path) = &options.output_file_path {
+    output_file_path = PathBuf::from(&output_path);
+  } else {
+    output_file_path = get_output_path(&options.input_file_path);
+  }
+
+  let output = Command::new("./ffmpeg.exe")
+    .arg("-i")
+    .arg(&options.input_file_path)
+    .arg("-filter:v")
+    .arg(&crop_dimensions)
+    .arg(&output_file_path)
+    .arg("-loglevel")
+    .arg("error")
+    .output();
+
+  let status = match output {
+    Ok(ref output) => output.status,
+    Err(error) => {
+      return Err(format!("There was an error while running the ffmpeg command: {}", error));
     }
+  };
 
-    let output = Command::new("./ffmpeg.exe")
-        .arg("-i")
-        .arg(&options.input_file_path)
-        .arg("-filter:v")
-        .arg(&crop_dimensions)
-        .arg(&output_file_path)
-        .arg("-loglevel")
-        .arg("error")
-        .output();
-        
+  if !status.success() {
+    let error_message = format!(
+      "{}\n{}",
+      status.to_string(),
+      std::str::from_utf8(&output.unwrap().stderr).unwrap()
+    );
+    return Err(error_message);
+  }
 
-    if output.is_err() {
-        return Err(output.unwrap_err().to_string());
-    }
-
-    let status = output.as_ref().unwrap().status;
-    if !status.success() {
-        let error_message = format!("{}\n{}", status.to_string(), std::str::from_utf8(&output.unwrap().stderr).unwrap());
-        return Err(error_message);
-    }
-    
-    return Ok(output_file_path.to_string_lossy().to_string());
-    }
-
+  return Ok(output_file_path.to_string_lossy().to_string());
+}
--- src/web/crop_image.rs
+++ src/web/crop_image.rs
@@ -1,10 +1,24 @@
 pub mod routes {
-    use drag_and_crop::{CropOptions, crop_image};
-    use rocket::serde::json::{Json, json, Value};
+  use drag_and_crop::{ CropOptions, crop_image, ApiResponse };
+  use rocket::serde::json::{ Json, json, Value };
 
-    #[post("/crop-image", format = "json", data = "<options>")]
-    pub fn post_crop_image(options: Json<CropOptions>) -> Value {
-        let result = crop_image(&options);
-        json!(result)
+  #[post("/crop-image", format = "json", data = "<options>")]
+  pub fn post_crop_image(options: Json<CropOptions>) -> Value {
+    let result = crop_image(&options);
+
+    match result {
+      Ok(result) =>
+        json!(ApiResponse {
+          success: true,
+          message: None,
+          data: Some(result),
+        }),
+      Err(error) =>
+        json!(ApiResponse::<String> {
+          success: false,
+          message: Some(error),
+          data: None,
+        }),
     }
+  }
 }
