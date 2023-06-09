6464a747e3ac83f1a2f827a55d3b5c327affbd58
DoubleDebug <duledodicc@gmail.com>
2023-06-09 18:04:44 +0200

    docs - added swagger ui

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ pub mod utils {
   pub mod init;
 }
 use utils::init::init;
+use rocket::fs::{ FileServer, Options };
 use crate::web::endpoints::routes::{ post_crop_image, post_crop_video };
 use crate::web::catchers::{ default_catcher, unprocessable_entity };
 
@@ -19,6 +20,8 @@ fn rocket() -> _ {
 
   rocket
     ::build()
+    .mount("/", FileServer::new("./static/swagger-ui", Options::Index).rank(1))
+    .mount("/docs", FileServer::new("./docs", Options::Index).rank(2))
     .mount("/api", routes![post_crop_image, post_crop_video])
     .register("/api", catchers![default_catcher, unprocessable_entity])
 }
